ebe37856b8ac34e5c9e1f89a2683cfc81090f84e
Andreas Böttger <andreas.boettger@tuhh.de>
2021-02-27 09:45:48 +0000

    Update start.sh

--- 4/start.sh
+++ 4/start.sh
@@ -21,7 +21,7 @@ chown -Rf www-data.www-data "/var/www/html"
 perl -i -ne 'print unless /^# INJECTION MARKER START/ .. /^# INJECTION MARKER END/' /etc/crontab
 {
   echo '# INJECTION MARKER START'
-  echo "5 * * * * www-data /usr/bin/php /var/www/html/console core:archive --url=${MATOMO_BASE_URL}"
+  echo "# 5 * * * * www-data /usr/bin/php /var/www/html/console core:archive --url=${MATOMO_BASE_URL}"
   echo '# INJECTION MARKER END'
 } >>/etc/crontab
 
